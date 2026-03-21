728b94078e2ab6514def866d8066b16b6fef0117
hefeicoder <hefeicoder@gmail.com>
2026-03-21 14:21:43 -0700

    feat: add app icon, generate .icns in package.sh

--- package.sh
+++ package.sh
@@ -19,6 +19,22 @@ mkdir -p "${APP_BUNDLE}/Contents/Resources"
 
 cp "${BUILD_DIR}/${APP_NAME}" "${APP_BUNDLE}/Contents/MacOS/${APP_NAME}"
 
+echo "==> Generating icon..."
+ICONSET="/tmp/AppIcon.iconset"
+rm -rf "${ICONSET}" && mkdir "${ICONSET}"
+sips -z 16 16     icon.png --out "${ICONSET}/icon_16x16.png"     > /dev/null
+sips -z 32 32     icon.png --out "${ICONSET}/icon_16x16@2x.png"  > /dev/null
+sips -z 32 32     icon.png --out "${ICONSET}/icon_32x32.png"     > /dev/null
+sips -z 64 64     icon.png --out "${ICONSET}/icon_32x32@2x.png"  > /dev/null
+sips -z 128 128   icon.png --out "${ICONSET}/icon_128x128.png"   > /dev/null
+sips -z 256 256   icon.png --out "${ICONSET}/icon_128x128@2x.png"> /dev/null
+sips -z 256 256   icon.png --out "${ICONSET}/icon_256x256.png"   > /dev/null
+sips -z 512 512   icon.png --out "${ICONSET}/icon_256x256@2x.png"> /dev/null
+sips -z 512 512   icon.png --out "${ICONSET}/icon_512x512.png"   > /dev/null
+cp icon.png "${ICONSET}/icon_512x512@2x.png"
+iconutil -c icns "${ICONSET}" -o "${APP_BUNDLE}/Contents/Resources/AppIcon.icns"
+rm -rf "${ICONSET}"
+
 cat > "${APP_BUNDLE}/Contents/Info.plist" << EOF
 <?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
@@ -44,6 +60,8 @@ cat > "${APP_BUNDLE}/Contents/Info.plist" << EOF
     <true/>
     <key>LSMinimumSystemVersion</key>
     <string>13.0</string>
+    <key>CFBundleIconFile</key>
+    <string>AppIcon</string>
     <key>NSHumanReadableCopyright</key>
     <string>Copyright © 2026 hefeicoder. MIT License.</string>
 </dict>
